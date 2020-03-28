234b4b5d94f51a221a07e8b3c13bcba400a134d0
José Arturo García López <jarturogl05@gmail.com>
2020-03-27 18:02:11 -0600

    Programa

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace procesamiento_asincrono_jarturogl05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( EscribirHolaAsync().Status);
+        }
+
+
+        static async Task<Task> EscribirHolaAsync()
+        {
+          await HacerCosasAsync();
+
+          return Task.CompletedTask;
+        }
+
+        static async Task HacerCosasAsync()
+        {
+
+            Thread.Sleep(5000);
+            Console.WriteLine("Hola");
+        }
+    }
+}
